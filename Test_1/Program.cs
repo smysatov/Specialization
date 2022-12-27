393d7c14e579cd14f5ad9f7fba150b1805fb905d
Sergey <smysatov@gmail.com>
2022-12-27 21:10:13 +0300

    SortArray done

--- Test_1/Program.cs
+++ Test_1/Program.cs
@@ -4,9 +4,15 @@
 //выполнения алгоритма. При решении не рекомендуется пользоваться
 //коллекциями, лучше обойтись исключительно массивами.
 
-string[] array = FillArray();
-PrintArray(array);
+string[] array1 = FillArray();
+Console.Write("Ваш Массив: ");
+PrintArray(array1);
 Console.WriteLine();
+
+string[] array2 = new string[array1.Length];
+SortArray(array1, array2);
+Console.Write("Ваш новый массив из эл. длинной не более 3 символов: ");
+PrintArray(array2);
 
 string[] FillArray()
 {
@@ -14,12 +20,24 @@ string[] FillArray()
     return Console.ReadLine().Split(" ");
 }
 
+void SortArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+}
+
 void PrintArray(string[] array)
 {
     int length = array.Length;
-    Console.Write("Ваш Массив: ");
     for (int i = 0; i < length; i++)
     {
-        Console.Write($"{array[i]}, ");
+        Console.Write($"{array[i]} ");
     }
 }
